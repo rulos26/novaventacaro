84479f36c6fa113c327ea7b6766fc0aeec020965
juan.diaz <rulos26@gmail.com>
2025-01-10 09:17:18 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Http\Controllers\RegisteredUserController;
 
 Route::get('/login', function () {
     return view('auth.login');
 })->name('login');
 
+Route::middleware(['web'])->group(function () {
+    Route::get('/register', [RegisteredUserController::class, 'create'])->name('register');
+});
