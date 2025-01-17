e6996eadd881f98c96676b6d57a6fb455944f886
rulos26 <rulos26@gmail.com>
2025-01-17 16:05:50 +0000

    crud

---
+++ database/migrations/2025_01_17_155221_create_pruebas_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pruebas', function (Blueprint $table) {
+            
+            $table->id(); // Columna ID primaria autoincremental
+                        $table->string('nombre'); // Columna de texto para el nombre
+                                    $table->text('descripcion')->nullable(); // Columna de texto largo para descripción (opcional)
+                                                $table->timestamps(); // Columnas created_at y updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pruebas');
+    }
+};
